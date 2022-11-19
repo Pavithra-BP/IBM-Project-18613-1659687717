05fd1047969c561d113ceb7bdac8163fed3c6643
itzz-aishwarya <111132819+itzz-aishwarya@users.noreply.github.com>
2022-11-19 22:49:55 +0530

    Update database.py

--- SFRA/database.py
+++ SFRA/database.py
@@ -13,7 +13,7 @@ def updateTableData(Firstname,Lastname,EmailID,password):
     sql = "UPDATE userdetails SET ( Firstname,Lastname,email,password)=('{}','{}','{}') WHERE rollno={}".format(Firstname,Lastname,EmailID,password) out = ibm_db.exec_immediate(conn,sql) 
 print('Number of affected rows : ', ibm_db.num_rows(out), "\n")
 try:
-    conn=ibm_db.connect("DATABASE=bludb;HOSTNAME=2d46b6b4-cbf6-40eb-bbce-6251e6ba0300.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=32328;SECURITY=SSL;SSLServerCertificate=DigiCertGlo balRootCA.crt;PROTOCOL=TCPIP;UID=bjn03696;PWD=ef96tLJX2VjzaCPX;", "", "")
+    conn=ibm_db.connect("DATABASE=bludb;HOSTNAME=2d46b6b4-cbf6-40eb-bbce-6251e6ba0300.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=32328;SECURITY=SSL;SSLServerCertificate=DigiCertGlo balRootCA.crt;PROTOCOL=TCPIP;UID=qbz18664,PWD=tg7qo042j5KSDA4i;", "", "")
 print("Db connected")
 except:
     print("Error")
